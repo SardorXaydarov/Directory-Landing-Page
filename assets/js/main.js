8bde19fd1b204cb02b2a1758e168ea85ce6bf522
SardorXaydarov <sardorxaydarovyt>
2023-12-17 18:57:42 +0500

    review section finished desktop first

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    slidesPerView: 4,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop:Infinity,
+  });
